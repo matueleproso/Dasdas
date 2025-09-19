17d756a41d16bd4b97063aabb1f8dc9bd4be67cf
jjisgay527 <programadormovel@gmail.com>
2025-09-19 08:30:35 -0300

    não pode comer pedra

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,5 +1,10 @@
+import axios from "axios"
 const Produtos = () => {
+axios.get("http://viacep.com.br/ws/01010000/json")
+    .then(response=>{ 
+    console.log(response.data)
 
+})
     const pizzas = [
         'Pizza de Mularela',
         'Pizza de Calabresa',
