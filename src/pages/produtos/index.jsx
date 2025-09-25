11256128b5ccb3639d46ec5c1a61b49bfd6992ae
jjisgay527 <programadormovel@gmail.com>
2025-09-25 07:48:03 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,17 +1,23 @@
+import { useState, useEffect } from "react"
+// Hooks do react - estado e renderização
 import axios from "axios"
+
 const Produtos = () => {
+    // Estado com lista de pizzas
+    const [pizzas, setPizzas] = useState(['Calabresa', 'Muçarela', 'Baiana'])
+
+    // Consumo de endpoint com as pizzas
+    // GET - busca os dados do endpoint ou rota
+    // THEN - é uma Promise - método assíncrono
+    // catch - tratamento de erros
 axios.get("http://viacep.com.br/ws/01010000/json")
     .then(response=>{ 
-    console.log(response.data)
-
+    console.log(response.data.data)
+    setPizzas(response.data.data)
 })
-    const pizzas = [
-        'Pizza de Mularela',
-        'Pizza de Calabresa',
-        'Pizza Baiana'
-    ]
-
-    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+.catch(errors=>console.log(errors))
+    const listaPizzas = pizzas.map(pizza=><li>{pizza.nome}</li>)
+    // Iteração da lista de pizzas
     return (
         <div>
             <ul>
